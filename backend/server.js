d2eef42eda8999480c4a060cd1070656941b4743
Jenny <jennywei0102@gmail.com>
2025-08-20 17:34:21 +0200

    Add routes used in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import formsRouter from "./routes/forms.js";
+import creatorsRouter from "./routes/creators.js";
+import inviteTokensRouter from "./routes/inviteTokens.js";
 
 dotenv.config();
 
@@ -7,6 +10,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use("/api/forms", formsRouter);
+app.use("/api/creators", creatorsRouter);
+app.use("/api/inviteTokens", inviteTokensRouter);
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT} 🚀`);
 });
