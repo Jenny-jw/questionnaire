63afad65278841c9cf5777904ee093758853d71c
Jenny <jennywei0102@gmail.com>
2025-08-26 11:01:35 +0200

    Update form model

--- backend/models/Form.js
+++ backend/models/Form.js
@@ -11,10 +11,13 @@ const formSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   fileds: [filedSchema],
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "Creater" },
   requireLogin: { type: Boolean, default: false },
-  allowAnonymouse: { type: Boolean, default: false },
+  allowAnonymous: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "Creator" },
+  ownerEmail: { type: String },
+  ownerTokenHash: { type: String },
+  ownerTokenExpireAt: { type: Date },
 });
 
 export default mongoose.model("Form", formSchema);
