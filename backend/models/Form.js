b70c1e1af147af993bbced30cd5b0ddff79f046e
Jenny <jennywei0102@gmail.com>
2025-09-11 15:07:01 +0200

    Fix typo

--- backend/models/Form.js
+++ backend/models/Form.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const filedSchema = new mongoose.Schema({
+const fieldSchema = new mongoose.Schema({
   question: { type: String, required: true },
   type: {
     type: String,
@@ -14,7 +14,7 @@ const filedSchema = new mongoose.Schema({
 const formSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
-  fileds: [filedSchema],
+  fields: [fieldSchema],
   requireLogin: { type: Boolean, default: false },
   allowAnonymous: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
