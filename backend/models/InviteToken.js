b3f5f168d6a5e85e626c213a9ab80eba88482331
Jenny <jennywei0102@gmail.com>
2026-01-25 10:16:29 +0100

    Change token into inviteToken in InviteToken schema

--- backend/models/InviteToken.js
+++ backend/models/InviteToken.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const inviteTokenSchema = new mongoose.Schema({
   form: { type: mongoose.Schema.Types.ObjectId, ref: "Form", required: true },
-  token: { type: String, required: true, unique: true },
+  inviteToken: { type: String, required: true, unique: true },
   expiresAt: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
 });
